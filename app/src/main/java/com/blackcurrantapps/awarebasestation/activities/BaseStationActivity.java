c0a44c531eba94d51fbb366ba6dfdf35c529b10a
Sanket <sanket.berde@gmail.com>
2017-01-29 00:58:11 +0530

    Basestation App Almost done

--- app/src/main/java/com/blackcurrantapps/awarebasestation/activities/BaseStationActivity.java
+++ app/src/main/java/com/blackcurrantapps/awarebasestation/activities/BaseStationActivity.java
@@ -1,17 +1,50 @@
 package com.blackcurrantapps.awarebasestation.activities;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blackcurrantapps.awarebasestation.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BaseStationActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.device_number)
+    TextView deviceNumber;
+    private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
+    private Handler mHandler;
+    private static final long SCAN_PERIOD = 10000;
+    private BluetoothLeScanner mLEScanner;
+    private FirebaseDatabase mFirebaseDatabase;
 
     @BindView(R.id.textView2)
     TextView textView2;
@@ -24,7 +57,8 @@ public class BaseStationActivity extends AppCompatActivity {
     @BindView(R.id.activity_base_station)
     RelativeLayout activityBaseStation;
 
-    private String rideName = "thunder";
+    private String rideKey = "thunder";
+    private String rideName = "Thunder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +66,194 @@ public class BaseStationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_base_station);
         ButterKnife.bind(this);
 
+        rideKey = getIntent().getStringExtra("RIDE_KEY");
         rideName = getIntent().getStringExtra("RIDE_NAME");
+
+        if (savedInstanceState == null) {
+            try {
+                FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+                FirebaseDatabase.getInstance().getReference("rides").child(rideKey).keepSynced(true);
+            } catch (Exception ignore) {
+            }
+        }
+
+        getSupportActionBar().setTitle("Base Station : " + rideKey);
+
+        mHandler = new Handler();
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE Not Supported",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+            scanLeDevice(true);
+        }
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        mFirebaseDatabase.getReference().child("rides").child(rideKey).child("nearby_devices").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mFirebaseDatabase.getReference().child("rides").child(rideKey).child("current_nearby_devices_count").setValue(dataSnapshot.getChildrenCount());
+                deviceNumber.setText(String.valueOf(dataSnapshot.getChildrenCount()));
+                Iterable<DataSnapshot> devices = dataSnapshot.getChildren();
+                String names = "";
+                for (DataSnapshot device : devices) {
+                    names = names + "\n" + device.getKey();
+                }
+                devicesTextView.setText(names);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                forceCleanup();
+            }
+        },30000);
+    }
+
+    private void forceCleanup(){
+
+        final DatabaseReference mref = mFirebaseDatabase.getReference().child("rides").child(rideKey);
+
+        mref.child("nearby_devices").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> devices = dataSnapshot.getChildren();
+                for (DataSnapshot device : devices) {
+                    Long updateTime = (Long) device.getValue();
+                    if (System.currentTimeMillis() - updateTime > 60000) {
+                        mref.child("nearby_devices").child(device.getKey()).setValue(null);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                forceCleanup();
+            }
+        },30000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+            scanLeDevice(false);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Bluetooth not enabled.
+                finish();
+                return;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            mLEScanner.startScan(new ArrayList<ScanFilter>(),
+                    new ScanSettings.Builder()
+                            .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+                            .build(), mScanCallback);
+
+            searchStatus.setText("SCANNING");
+
+        } else {
+            mLEScanner.stopScan(mScanCallback);
+        }
+    }
+
+    private void registerDevices(ArrayList<String> devices) {
+        final DatabaseReference mref = mFirebaseDatabase.getReference().child("rides").child(rideKey);
+
+        DatabaseReference macIdRef = mFirebaseDatabase.getReference().child("mac_id");
+
+        for (String mac : devices) {
+            mref.child("nearby_devices").child(mac).setValue(System.currentTimeMillis());
+            macIdRef.child(mac).child("nearest_base_station_key").setValue(rideKey);
+            macIdRef.child(mac).child("nearest_base_station_name").setValue(rideName);
+            macIdRef.child(mac).child("update_timestamp").setValue(System.currentTimeMillis());
+        }
+
+        mref.child("nearby_devices").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> devices = dataSnapshot.getChildren();
+                for (DataSnapshot device : devices) {
+                    Long updateTime = (Long) device.getValue();
+                    if (System.currentTimeMillis() - updateTime > 60000) {
+                        mref.child("nearby_devices").child(device.getKey()).setValue(null);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            Log.i("callbackType", String.valueOf(callbackType));
+            Log.i("result", result.toString());
+            BluetoothDevice btDevice = result.getDevice();
+            Log.v("Found Device", btDevice.getAddress());
+            ArrayList<String> uids = new ArrayList<>();
+            uids.add(btDevice.getAddress());
+            registerDevices(uids);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            ArrayList<String> uids = new ArrayList<>();
+            for (ScanResult sr : results) {
+                BluetoothDevice btDevice = sr.getDevice();
+                Log.v("Found Device", btDevice.getAddress());
+                uids.add(btDevice.getAddress());
+            }
+            registerDevices(uids);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+
+    };
 }
--- app/src/main/java/com/blackcurrantapps/awarebasestation/activities/MainActivity.java
+++ app/src/main/java/com/blackcurrantapps/awarebasestation/activities/MainActivity.java
@@ -1,23 +1,14 @@
 package com.blackcurrantapps.awarebasestation.activities;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanFilter;
-import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.blackcurrantapps.awarebasestation.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -26,186 +17,75 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-    private BluetoothAdapter mBluetoothAdapter;
-    private int REQUEST_ENABLE_BT = 1;
-    private Handler mHandler;
-    private static final long SCAN_PERIOD = 10000;
-    private BluetoothLeScanner mLEScanner;
+
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
+    @BindView(R.id.listView)
+    ListView listView;
     private FirebaseDatabase mFirebaseDatabase;
-    private Boolean firebaseInit = false;
+
+    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> keys = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHandler = new Handler();
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "BLE Not Supported",
-                    Toast.LENGTH_SHORT).show();
-            finish();
-        }
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        mBluetoothAdapter = bluetoothManager.getAdapter();
+        ButterKnife.bind(this);
 
-        if (savedInstanceState == null) {
-            try {
-                FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-                FirebaseDatabase.getInstance().getReference("rides").child(rideName).keepSynced(true);
-            } catch (Exception ignore) {
-            }
-        }
+        getSupportActionBar().setTitle("Select Base Station");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        } else {
-            mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
-            scanLeDevice(true);
-        }
 
         FirebaseAuth.getInstance().signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 mFirebaseDatabase = FirebaseDatabase.getInstance();
-                firebaseInit = true;
+
+                mFirebaseDatabase.getReference().child("ride_names").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Iterable<DataSnapshot> rides = dataSnapshot.getChildren();
+
+                        for (DataSnapshot ride:rides){
+                            names.add((String) ride.child("name").getValue());
+                            keys.add((String) ride.child("key").getValue());
+                        }
+
+                        listView.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,names));
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                Intent intent = new Intent(MainActivity.this,BaseStationActivity.class);
+                                intent.putExtra("RIDE_KEY",keys.get(i));
+                                intent.putExtra("RIDE_NAME",names.get(i));
+                                startActivity(intent);
+                                finish();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
-            scanLeDevice(false);
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode == Activity.RESULT_CANCELED) {
-                //Bluetooth not enabled.
-                finish();
-                return;
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void scanLeDevice(final boolean enable) {
-        if (enable) {
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mLEScanner.stopScan(mScanCallback);
-                    Log.v("SCAN_STATUS","STOPPED SCAN");
-                }
-            }, SCAN_PERIOD);
-
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mLEScanner.startScan(mScanCallback);
-                    Log.v("SCAN_STATUS","STARTED SCAN");
-                }
-            }, SCAN_PERIOD + 5000);
-
-            mLEScanner.startScan(new ArrayList<ScanFilter>() ,
-                    new ScanSettings.Builder()
-                        .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-                        .build() , mScanCallback);
-
-            Log.v("SCAN_STATUS","STARTED SCAN");
-
-        } else {
-            mLEScanner.stopScan(mScanCallback);
-        }
-    }
-
-    private final String rideName = "thunder";
-
-    private void registerDevices(ArrayList<String> devices){
-        if (firebaseInit){
-            final DatabaseReference mref = mFirebaseDatabase.getReference().child("rides").child(rideName);
-
-            for (String mac : devices){
-                mref.child("nearby_devices").child(mac).setValue(System.currentTimeMillis());
-            }
-
-            mref.child("nearby_devices").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Iterable<DataSnapshot> devices = dataSnapshot.getChildren();
-                    for (DataSnapshot device : devices){
-                        Long updateTime = (Long) device.getValue();
-                        if (System.currentTimeMillis()-updateTime < 60000){
-                            mref.child("nearby_devices").child(device.getKey()).setValue(null);
-                        }
-                    }
-                    mref.child("nearby_devices").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            mref.child("current_nearby_devices_count").setValue(dataSnapshot.getChildrenCount());
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-
-                        }
-                    });
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
-        }
-    }
-
-    private ScanCallback mScanCallback = new ScanCallback() {
-
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            Log.i("callbackType", String.valueOf(callbackType));
-            Log.i("result", result.toString());
-            BluetoothDevice btDevice = result.getDevice();
-            Log.v("Found Device",btDevice.getAddress());
-            ArrayList<String> uids = new ArrayList<>();
-            uids.add(btDevice.getAddress());
-            registerDevices(uids);
-        }
-
-        @Override
-        public void onBatchScanResults(List<ScanResult> results) {
-            ArrayList<String> uids = new ArrayList<>();
-            for (ScanResult sr : results) {
-                BluetoothDevice btDevice = sr.getDevice();
-                Log.v("Found Device",btDevice.getAddress());
-                uids.add(btDevice.getAddress());
-            }
-            registerDevices(uids);
-        }
-
-        @Override
-        public void onScanFailed(int errorCode) {
-            Log.e("Scan Failed", "Error Code: " + errorCode);
-        }
-
-    };
 }
