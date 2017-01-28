a9f5d4cd653e2dad7cfcab6c697e05af68a83bff
Sanket <sanket.berde@gmail.com>
2017-01-28 17:21:02 +0530

    basic setup

---
+++ app/src/main/java/com/blackcurrantapps/awarebasestation/Global.java
@@ -0,0 +1,17 @@
+package com.blackcurrantapps.awarebasestation;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Sanket on 28/01/17.
+ * Copyright (c) BlackcurrantApps LLP.
+ */
+
+public class Global extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
--- app/src/main/java/com/blackcurrantapps/awarebasestation/activities/MainActivity.java
+++ app/src/main/java/com/blackcurrantapps/awarebasestation/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.blackcurrantapps.awarebasestation;
+package com.blackcurrantapps.awarebasestation.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blackcurrantapps.awarebasestation.R;
 
 public class MainActivity extends AppCompatActivity {
 
